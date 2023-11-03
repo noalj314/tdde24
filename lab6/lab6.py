abdc04c07d3da332bce3dca066400c95e2d30e95
noalj314 <noalj314@student.liu.se>
2023-11-03 11:59:20 +0100

    updated lab6.py

--- lab6/lab6.py
+++ lab6/lab6.py
@@ -1,5 +1,6 @@
 from calc import *
 
+
 def exec_program(lst):
     if is_program(lst):
         statements = program_statements(lst)
@@ -13,7 +14,19 @@ def exec_program(lst):
             if is_constant(statement):
                 return statement
 
+
 def exec_statement(lst):
+    if isinstance(lst, list) and isinstance(lst[0], list):
+        for statement in lst:
+            if is_output(statement):
+                print_statement(statement)
+            if is_selection(statement):
+                if_statement(statement)
+            if is_binaryexpr(statement):
+                binary_statement(statement)
+            if is_constant(statement):
+                return statement
+    else:
         if is_output(lst):
             print_statement(lst)
         if is_selection(lst):
@@ -23,51 +36,50 @@ def exec_statement(lst):
         if is_constant(lst):
             return lst
 
+
 def print_statement(print_list):
-        print(binary_statement(output_expression(print_list)))
+    print(output_expression(print_list))
+
+
+calc1 = ['calc', [4]]
+# print_list = ['print', 2 + 4]
+
+
 
 def binary_statement(lst):
     result = None
-    if isinstance(lst,int):
-        return lst
-    elif binaryexpr_operator(lst) == '+':
-            result = binary_statement(binaryexpr_left(lst)) + binary_statement(binaryexpr_right(lst))
-    elif binaryexpr_operator(lst) == '-':
-            result = binary_statement(binaryexpr_left(lst)) - binary_statement(binaryexpr_right(lst))
-    elif binaryexpr_operator(lst) == '/':
-            result = binary_statement(binaryexpr_left(lst)) / binary_statement(binaryexpr_right(lst))
-    elif binaryexpr_operator(lst) == '*':
-            result = binary_statement(binaryexpr_left(lst)) * binary_statement(binaryexpr_right(lst))
+    if binaryexpr_operator(lst) == "+":
+        result = binaryexpr_left(lst) + binaryexpr_right(lst)
+    elif binaryexpr_operator(lst) == "-":
+        result = binaryexpr_left(lst) - binaryexpr_right(lst)
+    elif binaryexpr_operator(lst) == "/":
+        result = binaryexpr_left(lst) / binaryexpr_right(lst)
+    elif binaryexpr_operator(lst) == "*":
+        result = binaryexpr_left(lst) * binaryexpr_right(lst)
     return result
+
 
 def if_statement(if_list):
     if is_condition(if_list[1]):
-        new = if_list[1] 
-        if condition_operator(new) == '>' and binary_statement(condition_left(new)) > binary_statement(condition_right(new)):
-            return exec_statement(if_list[2])
-        if condition_operator(new) == '<' and binary_statement(condition_left(new)) < binary_statement(condition_right(new)):
-            return exec_statement(if_list[2])
-        if condition_operator(new) == '=' and binary_statement(condition_left(new)) == binary_statement(condition_right(new)):
-            return exec_statement(if_list[2])
-        if len(if_list) == 4:
-            return exec_statement(if_list[3])
-            
+        new = if_list[1]
+        if condition_operator(new) == '>':
+            if exec_statement(condition_left(new)) > exec_statement(condition_right(new)):
+                exec_statement(if_list[2:])
+            else:
+                exec_statement(if_list[3])
+        if condition_operator(new) == '<':
+            if exec_statement(condition_left(new)) < exec_statement(condition_right(new)) :
+                exec_statement(if_list[2:])
+            else:
+                exec_statement(if_list[3])
+        if condition_operator(new) == '=':
+            if exec_statement(condition_left(new)) == exec_statement(condition_right(new)):
+                exec_statement(if_list[2:])
+            else:
+                exec_statement(if_list[3])
 
 
-calc2 = ['calc', ['if', [[[[2, '*', 3], '-', 1], '+', 1], '=', 6], ['print', 2], ['print', 4]]]
+calc2 = ['calc', ['if', [[[2,"+",3], "+", 5], '>', 5], ['print', 2], ['print', 4]]]
 calc3 = ['calc', ['if', [3, '=', 5], ['print', 2], ['print', 4]]]
-['if', [8, '>', 5], ['print', 2]]
 
-# STATEMENTS = STATEMENT, STATEMENTS
-
-# STATEMENT = SELECTION | OUTPUT 
-
-# binaryexpr = [['EXPR', '+', 'EXPR'], '+', 'EXPR']
-
-# EXPR = 5 or binaryexpr
-
-# OUTPUT = '[', "'print'", EXPRESSION, ']' 
-
-calc4 = ['calc', ['print', [[[5, '-', [1, '+', 2]], '+', 2], '+', [2, '*', 3]]], ['if', [[[[2, '*', 3], '-', 1], '+', 1], '=', 6], ['print', 2], ['print', 4]], ['if', [3, '=', 5], ['print', 2]]]
-
-exec_program(calc4)
+exec_program(calc2)
