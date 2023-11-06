4daea8b5f88a6457f125e9354fa57ad8b8928adb
noalj314 <noalj314@student.liu.se>
2023-11-06 10:32:41 +0100

    updated lab6.py

--- lab6/lab6.py
+++ lab6/lab6.py
@@ -5,36 +5,18 @@ def exec_program(lst):
     if is_program(lst):
         statements = program_statements(lst)
         for statement in statements:
-            if is_output(statement):
-                print_statement(statement)
-            if is_selection(statement):
-                if_statement(statement)
-            if is_binaryexpr(statement):
-                binary_statement(statement)
-            if is_constant(statement):
-                return statement
+            exec_statement(statement)
 
 
 def exec_statement(lst):
-    if isinstance(lst, list) and isinstance(lst[0], list):
-        for statement in lst:
-            if is_output(statement):
-                print_statement(statement)
-            if is_selection(statement):
-                if_statement(statement)
-            if is_binaryexpr(statement):
-                binary_statement(statement)
-            if is_constant(statement):
-                return statement
-    else:
-        if is_output(lst):
-            print_statement(lst)
-        if is_selection(lst):
-            if_statement(lst)
-        if is_binaryexpr(lst):
-            return binary_statement(lst)
-        if is_constant(lst):
-            return lst
+    if is_output(lst):
+        print_statement(lst)
+    elif is_selection(lst):
+        if_statement(lst)
+    elif is_binaryexpr(lst):
+        return binary_statement(lst)
+    elif is_constant(lst):
+        return lst
 
 
 def print_statement(print_list):
@@ -45,11 +27,12 @@ calc1 = ['calc', [4]]
 # print_list = ['print', 2 + 4]
 
 
-
+# The binary_statement function assumes that the left and right sides of a binary expression are always constants, but they could also be variables or other expressions based on the grammar.
 def binary_statement(lst):
     result = None
     if binaryexpr_operator(lst) == "+":
-        result = binaryexpr_left(lst) + binaryexpr_right(lst)
+        print(exec_statement(binaryexpr_left(lst)))
+        result = exec_statement(binaryexpr_left(lst)) + exec_statement(binaryexpr_right(lst))
     elif binaryexpr_operator(lst) == "-":
         result = binaryexpr_left(lst) - binaryexpr_right(lst)
     elif binaryexpr_operator(lst) == "/":
@@ -60,26 +43,28 @@ def binary_statement(lst):
 
 
 def if_statement(if_list):
-    if is_condition(if_list[1]):
-        new = if_list[1]
-        if condition_operator(new) == '>':
-            if exec_statement(condition_left(new)) > exec_statement(condition_right(new)):
+
+    if is_binaryexpr(selection_condition(if_list)):
+        if_condition = if_list[1]
+        print(if_list)
+        if condition_operator(if_condition) == '>':
+            if exec_statement(condition_left(if_condition)) > exec_statement(condition_right(if_condition)):
+                print("pring")
+            else:
+                exec_statement(if_list[3])
+        if condition_operator(if_condition) == '<':
+            if exec_statement(condition_left(if_condition)) < exec_statement(condition_right(if_condition)) :
                 exec_statement(if_list[2:])
             else:
                 exec_statement(if_list[3])
-        if condition_operator(new) == '<':
-            if exec_statement(condition_left(new)) < exec_statement(condition_right(new)) :
-                exec_statement(if_list[2:])
-            else:
-                exec_statement(if_list[3])
-        if condition_operator(new) == '=':
-            if exec_statement(condition_left(new)) == exec_statement(condition_right(new)):
+        if condition_operator(if_condition) == '=':
+            if exec_statement(condition_left(if_condition)) == exec_statement(condition_right(if_condition)):
                 exec_statement(if_list[2:])
             else:
                 exec_statement(if_list[3])
 
 
-calc2 = ['calc', ['if', [[[2,"+",3], "+", 5], '>', 5], ['print', 2], ['print', 4]]]
+calc2 = ['calc', ['if', [4, "+", 2], '>', 5], ['print', 2], ['print', 4]]
 calc3 = ['calc', ['if', [3, '=', 5], ['print', 2], ['print', 4]]]
 
 exec_program(calc2)
