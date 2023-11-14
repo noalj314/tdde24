aa3d053d8f467bada760c57cc0098d0fba4283f2
noalj314 <noalj314@student.liu.se>
2023-11-14 13:18:54 +0100

    Finished the code

--- lab6/lab6.py
+++ lab6/lab6.py
@@ -1,128 +1,147 @@
 from calc import *
+
+
 def exec_program(lst, dic=None):
     if dic is None:
         dic = {}
-    else:
-        dic = dic.copy()
     if is_program(lst):
         statements = program_statements(lst)
         for statement in statements:
-            exec_statement(statement, dic)
+            dic = exec_statement(statement, dic)
         return dic
     else:
         raise SyntaxError("Not a program")
+
 def exec_statement(lst, dic):
     if is_output(lst):
-        print_statement(lst, dic)
+        return print_statement(lst, dic)
     elif is_assignment(lst):
-        ass_statement(lst, dic)
+        return ass_statement(lst, dic)
     elif is_selection(lst):
-        if_statement(lst, dic)
+        return if_statement(lst, dic)
     elif is_input(lst):
-        inputer(lst, dic)
+        return inputer(lst, dic)
     elif is_repetition(lst):
-        whiler(lst,dic)
+        return whiler(lst, dic)
     elif is_condition(lst):
-        return if_condition(lst,dic)
+        return if_condition(lst, dic)
     elif is_variable(lst):
         return variable(lst, dic)
     elif is_binaryexpr(lst):
         return binary_statement(lst, dic)
     elif is_constant(lst):
         return lst
+    return dic
+
 
 def print_statement(print_list, dic):
     if output_expression(print_list) in dic:
         y = dic.get((output_expression(print_list)))
         print(output_expression(print_list) + " =", + y)
     else:
-        print(exec_statement(output_expression(print_list),dic))
-
-def inputer(lst,dic):
-    z = input()
-    if z == '':
-        raise ValueError("hej")
-    else:
-        z = int(z)
-    return dic.update({input_variable(lst): z})
-
-def variable(lst, dic):
-    return dic.get(lst)
-
-def ass_statement(statement, dic):
-    return dic.update({assignment_variable(statement): exec_statement(assignment_expression(statement), dic)})
-
-def whiler(lst,dic):
-    while exec_statement(repetition_condition(lst), dic):
-        for statement in repetition_statements(lst):
-            exec_statement(statement, dic)
+        print(exec_statement(output_expression(print_list), dic))
     return dic
 
-def binary_statement(lst, dic ):
+
+def inputer(lst, dic):
+    dic_local = dic.copy()
+    read_variable = input_variable(lst)
+    val = input(f"Enter value for {read_variable}: ")
+    int_val = int(val)
+    dic_local[read_variable] = int_val
+    return dic_local
+
+
+def variable(statement, dic):
+    variable_value = dic.get(statement)
+    return variable_value
+
+
+def ass_statement(statement, dic):
+    dic_local = dic.copy()
+    var = assignment_variable(statement)
+    value = exec_statement(assignment_expression(statement), dic_local)
+    dic_local[var] = value
+    return dic_local
+
+
+def whiler(lst, dic):
+    dic_local = dic.copy()
+    while exec_statement(repetition_condition(lst), dic_local):
+        for statement in repetition_statements(lst):
+            dic_local = exec_statement(statement, dic_local)
+    return dic_local
+
+
+def binary_statement(lst, dic):
     result = None
+    left = exec_statement(binaryexpr_left(lst), dic)
+    right = exec_statement(binaryexpr_right(lst), dic)
     if binaryexpr_operator(lst) == "+":
-        result = exec_statement(binaryexpr_left(lst), dic) + exec_statement(binaryexpr_right(lst), dic)
+        result = left + right
     elif binaryexpr_operator(lst) == "-":
-        result = exec_statement(binaryexpr_left(lst), dic) - exec_statement(binaryexpr_right(lst), dic)
+        result = left - right
     elif binaryexpr_operator(lst) == "/":
-        result = exec_statement(binaryexpr_left(lst), dic) / exec_statement(binaryexpr_right(lst) , dic)
+        if right == 0:
+            raise Exception(f"Division is by zero")
+        else:
+            result = left / right
     elif binaryexpr_operator(lst) == "*":
-        result = exec_statement(binaryexpr_left(lst), dic) * exec_statement(binaryexpr_right(lst) , dic)
+        result = left * right
     return result
 
-#calc2 = [['if', [10, '>', 5], ['print', 2], ['print', 4]]
 
 def if_condition(lst, dic):
+    left = exec_statement(condition_left(lst), dic)
+    right = exec_statement(condition_right(lst), dic)
     if condition_operator(lst) == '>':
-        if exec_statement(condition_left(lst), dic) > exec_statement(condition_right(lst), dic):
+        if left > right:
             return True
         else:
             return False
     if condition_operator(lst) == '<':
-        if exec_statement(condition_left(lst), dic) < exec_statement(condition_right(lst), dic):
+        if left < right:
             return True
         else:
             return False
     if condition_operator(lst) == '=':
-        if exec_statement(condition_left(lst), dic) == exec_statement(condition_right(lst), dic):
+        if left == right:
             return True
         else:
             return False
+    return dic
+
 
 def if_statement(if_list, dic):
     if is_condition(if_list[1]):
         true_print = if_list[2]
         false_print = if_list[3] if selection_has_false_branch(if_list) else None
         if if_condition(if_list[1], dic):
-            exec_statement(true_print, dic)
+            return exec_statement(true_print, dic)
         else:
-            exec_statement(false_print, dic)
+            return exec_statement(false_print, dic) if false_print else dic
+    return dic
 
 
-#calc1 = ['calc', ['set', 'a', 5], ['print', 'a']]
-#calc_set2 = ['calc', ['set', 'a', [[[5, '-', [1, '+', 2]], '+', 2], '+', [2, '*', 3]]], ['print', 'a']]
-#exec_program(calc_set2)
-#calc100 = ['calc', ['set', 'a', 5], ['print', 'a']]
-#new_table = exec_program(calc1)
-#exec_program(calc1)
-#my_table = {'a': 7}
-#new_table = exec_program(calc1, my_table)
-#print(my_table)
-calc10 = ['calc1', ['ife', [10, '>', 5], ['print', 4]]]
+calc10 = ['calc', ['if', [1, '>', 5], ['print', 4], ['print', 3]]]
 talc2 = ['calc', ['set', 'x', 7],
-             ['set', 'y', 12],
-            ['set', 'z', ['x', '+', 'y']],
-            ['print', 'z']]
+         ['set', 'y', 12],
+         ['set', 'z', ['x', '+', 'y']],
+         ['print', 'z']]
 
 calc3 = ['calc', ['read', 'p1'],
          ['set', 'p2', 47],
-            ['set', 'p3', 179],
-            ['set', 'result', [['p1', '+', 'p2'], '+', 'p3']],
-             ['print', 'result']]
+         ['set', 'p3', 179],
+         ['set', 'result', [['p1', '+', 'p2'], '+', 'p3']],
+         ['print', 'result']]
 calc4 = ['calc', ['read', 'n'],
-             ['set', 'sum', 0],
-             ['while', ['n', '>', 0],
-             ['set', 'sum', ['sum', '+', 'n']],
-             ['set', 'n', ['n', '-', 1]]],
-             ['print', 'sum']]
-#exec_program(calc10)
+         ['set', 'sum', 0],
+         ['while', ['n', '>', 0],
+          ['set', 'sum', ['sum', '+', 'n']],
+          ['set', 'n', ['n', '-', 1]]],
+         ['print', 'sum']]
+
+
+# new_table = exec_program(calc3, my_table)
+
+# print(my_table)
