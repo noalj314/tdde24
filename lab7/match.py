c524135a9246ba75d44d5efca7a34f0d87ae3f0e
Kevin Magron <kevma271@student.liu.se>
2023-11-17 11:28:00 +0000

    Update match.py

--- lab7/match.py
+++ lab7/match.py
@@ -1,21 +1,4 @@
-#from books import *
-db = [[['författare', ['john', 'zelle']],
-       ['titel', ['python', 'programming', 'an', 'introduction', 'to',
-                  'computer', 'science']],
-       ['år', 2010]],
-      [['författare', ['armen', 'asratian']],
-       ['titel', ['diskret', 'matematik']],
-       ['år', 2012]],
-      [['författare', ['j', 'glenn', 'brookshear']],
-       ['titel', ['computer', 'science', 'an', 'overview']],
-       ['år', 2011]],
-      [['författare', ['john', 'zelle']],
-       ['titel', ['data', 'structures', 'and', 'algorithms', 'using',
-                  'python', 'and', 'c++']],
-       ['år', 2009]],
-      [['författare', ['anders', 'haraldsson']],
-       ['titel', ['programmering', 'i', 'lisp']],
-       ['år', 1993]]]
+from books import *
 
 def match(seq, pattern):
     """
@@ -23,33 +6,55 @@ def match(seq, pattern):
     """
     if not pattern:
         return not seq
-    elif not seq and pattern != ['--']:
-        return False
-
-    if pattern[0] == '&':
-        return match(seq[1:], pattern[1:])
     elif pattern[0] == '--':
-        return match(seq, pattern[1:]) or (seq and match(seq[1:], pattern))
-    elif isinstance(seq[0], list) and isinstance(pattern[0], list):
-        return match(seq[0], pattern[0]) and match(seq[1:], pattern[1:])
+        if match(seq, pattern[1:]):
+            return True
+        elif not seq:
+            return False
+        else:
+            return match(seq[1:], pattern)
+    elif not seq:
+        return False
+    elif pattern[0] == '&':
+        return match(seq[1:], pattern[1:])
     elif seq[0] == pattern[0]:
         return match(seq[1:], pattern[1:])
+    elif isinstance(seq[0], list) and isinstance(pattern[0], list):
+        first = match(seq[0], pattern[0])
+        rest = match(seq[1:], pattern[1:])
+        return first and rest
     else:
         return False
 
-    
 
 def search(pattern, db):
-    matched_books = []
-    for book in db:
-        if match(book, pattern):
-            matched_books.append(book)
-    return matched_books
+    lst = []
+    for item in db: 
+        if match(item, pattern):
+            lst.append(item)
+    return lst
+ 
 
 
-print(search([['författare', ['&', 'zelle']], ['titel', ['--', 'python', '--']], ['år', '&']], db))
 
-print(search(['--', ['år', 2042], '--'], db))
 
-print(search(['--', ['titel', ['&', '&']], '--'], db))
+
+
+
+seq1 = [ [['författare', ['armen', 'asratian']],
+       ['titel', ['diskret', 'matematik']],
+       ['år', 2012]],
+       [['författare', ['john', 'zelle']],
+       ['titel', ['python', 'programming', 'an', 'introduction', 'to',
+                  'computer', 'science']],
+       ['år', 2010]]  
+]
+
+pattern1 = [['författare', ['&', 'zelle']], ['titel', ['--', 'python', '--']], ['år', '&']]
+
+seq2 = [['titel', ['python',], ['år', 2010]]]
+seq3 = [[['python']], ['år', '&']]
+
+
+print(search(pattern1,db))
 
