fc4b1b88d47c20623abb5718ce9f2342c094dec1
noalj314 <noalj314@student.liu.se>
2023-11-22 14:03:08 +0100

    Added docstrings

--- lab7/7b/lab7b.py
+++ lab7/7b/lab7b.py
@@ -14,62 +14,52 @@ def traverse(tree, inner_node_fn, leaf_fn, empty_tree_fn):
 def value_of_tree(tree):
     """Returns sum of key and values of left nodes squared"""
     def empty_tree_fn():
-     return 0
-
+        """Returns 0"""
+        return 0
     def leaf_fn(key):
+        """Returns key value squared"""
         return key**2
-
     def inner_node_fn(key, left_value, right_value):
+        """"Returns sum of key- and left-value"""
         return key + left_value
     return traverse(tree, inner_node_fn, leaf_fn, empty_tree_fn)
 
 def contains_key(key, tree):
     """Returns a boolean value """
     def empty_tree_fn():
+        """Returns false"""
         return False
     def leaf_fn(leaf):
+        """Returns boolean value depending on key and leaf values"""
         return key == leaf
     def inner_node_fn(middle, left_value, right_value):
+        """Returns boolean value depending on key- and subtree-values"""
         return key == middle or left_value or right_value
     return traverse(tree, inner_node_fn, leaf_fn, empty_tree_fn)
 
 def tree_size(tree):
     """Returns the size of the tree"""
     def empty_tree_fn():
+        """Returns 0 if tree is empty"""
         return 0
     def leaf_fn(leaf):
+        """Returns 1 if node is a leaf"""
         return 1 
     def inner_node_fn(middle, left_value, right_value):
+        """Returns the amount of all the inner nodes"""
         return 1 + left_value + right_value
     return traverse(tree, inner_node_fn, leaf_fn, empty_tree_fn)
 
 def tree_depth(tree):
     """Returns the depth of the tree"""
     def empty_tree_fn():
+        """Returns 0 if tree is empty"""
         return 0
     def leaf_fn(leaf):
+        """Returns 1 if tree is a leaf"""
         return 1
     def inner_node_fn(middle, left_value, right_value):
+        """Returns the maximum depth between left and right value"""
         return 1 + max(left_value, right_value)
     return traverse(tree, inner_node_fn, leaf_fn, empty_tree_fn)
 
-"""
-print(value_of_tree([6, 7, 8]))
-
-print(contains_key(6, [6, 7, 8]))
-
-print(contains_key(2, [6, 7, [[2, 3, 4], 0, []]]))
-
-print(contains_key(2, [[], 1, 5]))
-
-print(tree_size([2, 7, []]))
-
-print(tree_size([]))
-
-print(tree_size([[1, 2, []], 4, [[], 5, 6]]))
-
-print(tree_depth(9))
-
-print(tree_depth([1, 5, [10, 7, 14]]))
-
-"""
